d6eb9940d420ff798cb7c5f7a7a2d8e2f9d539be
Denis Shubin <denis.shubin@gmail.com>
2022-04-26 12:22:57 +0300

    refactor: componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,12 +5,10 @@ import axios from 'axios';
 import userPhoto from '../../assets/images/user.png'
 
 class Users extends React.Component<UsersPropsType> {
-
-    constructor(props: UsersPropsType) {
-        super(props);
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            this.props.setUsers(response.data.items)
-        })
+            this.props.setUsers(response.data.items);
+        });
     }
 
     render() {
