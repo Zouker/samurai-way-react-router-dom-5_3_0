4411d72497c11ea2f44b8608b9dec7355feda83e
Denis Shubin <denis.shubin@gmail.com>
2022-06-08 15:28:27 +0300

    refactor: reselect

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -1,26 +1,32 @@
 import {RootStateType} from './redux-store';
 import {UserType} from './users-reducer';
+import {createSelector} from 'reselect';
 
-export const getUsers = (state: RootStateType): Array<UserType> =>{
-return state.usersPage.users
+const getUsersSelector = (state: RootStateType): Array<UserType> => {
+    return state.usersPage.users
 }
 
-export const getPageSize = (state: RootStateType) =>{
+export const getUsers = createSelector(getUsersSelector, (users) => {
+
+    return users.filter(u => true)
+})
+
+export const getPageSize = (state: RootStateType) => {
     return state.usersPage.pageSize
 }
 
-export const getTotalUsersCount = (state: RootStateType) =>{
+export const getTotalUsersCount = (state: RootStateType) => {
     return state.usersPage.totalUsersCount
 }
 
-export const getCurrentPage = (state: RootStateType) =>{
+export const getCurrentPage = (state: RootStateType) => {
     return state.usersPage.currentPage
 }
 
-export const getIsFetching = (state: RootStateType) =>{
+export const getIsFetching = (state: RootStateType) => {
     return state.usersPage.isFetching
 }
 
-export const getFollowingInProgress = (state: RootStateType) =>{
+export const getFollowingInProgress = (state: RootStateType) => {
     return state.usersPage.followingInProgress
 }
